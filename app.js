36aa30aeb22a7bc76e2ab7ffdaf55840bd5e6697
Dani <dani@Danis-MBP.lan1>
2021-02-27 14:40:45 -0500

    sample code with appendFIleSync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "This file was created by node..");
+// fs.writeFileSync("notes.txt", "This file was created by node..");
+
+fs.appendFileSync("notes.txt", "\n This is message two.");
